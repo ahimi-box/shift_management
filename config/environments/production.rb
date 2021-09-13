2049a1f5adb88c28e550222657638b5aebd3d7ba
ahimi <testtestabe07@gmail.com>
2021-09-13 15:21:50 +0900

    production編集1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,16 @@ Rails.application.configure do
 
   # 追加
   config.action_mailer.default_url_options = { :Host => 'https://infinite-eyrie-12132.herokuapp.com/' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: "heroku.com",
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
+      
+  }
+
 end
